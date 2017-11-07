525af6e6027745b9b125be15a032a7763919c053
CAEmario <CAEAddress@gmail.com>
2017-11-07 14:28:42 +0100

    Event edited by Luigi Test

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -60,7 +60,7 @@ var init = function() {
     
     	    $('# id of 3th radio button').parent().contents().last()[0].textContent=data.answerC;
     
-    	    $('# id of 4th radio button').parent().contents().last()[0].textContent=data.answerD;
+    	    $('#radio_4').parent().contents().last()[0].textContent=data.answerD;
     
     	    $('# id of the video').attr('src',data.videolink);
     	
